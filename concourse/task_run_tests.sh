5cd7c9623f74e46c5425bde21b24bdde1a06cbd7
gschultzy <greg.schultz@emc.com>
2017-09-26 14:36:00 +1000

    updates post testing

--- concourse/task_run_tests.sh
+++ concourse/task_run_tests.sh
@@ -1,6 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-ls -al
+set -e # fail fast
+set -x # print commands
 
-ls -al ci-repo/
+# Show resource directories
+ls -al ci-repo/concourse
 ls -al ci-project/
+
+# Check node and npm versions
+node --version
+nodejs --version
+npm --version
+
+# Setup app dependencies
+cd ci-project/
+npm install
+
+# Install forever tool and start web app in background
+npm install forever -g
+forever start app/server.js
+forever list
+
+# Pause for server to app to start
+sleep 5
+
+# Run unit and intergartion tests
+npm test
